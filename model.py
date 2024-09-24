3c1d1dbb206b70cb60ea3951ef4e960c4271b234
TrungKhanhNguyen <khanhnt92@gmail.com>
2024-09-25 00:02:02 +0700

    Update model.py

--- model.py
+++ model.py
@@ -153,7 +153,7 @@ def train_model(token):
     X = np.array(range(len(df))).reshape(-1, 1)  # Sử dụng chỉ số thời gian làm đặc trưng
     y = df['close'].values  # Target: closing prices
 
-    model = SVR()
+    model = SVR(kernel='rbf')
     model.fit(X, y)
 
     next_time_index = np.array([[len(df)]])  # Giá trị thời gian tiếp theo
