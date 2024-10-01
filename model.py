557165a5a487ac10911021ceb415f8be623659ed
TrungKhanhNguyen <khanhnt92@gmail.com>
2024-10-01 21:54:11 +0700

    Update model.py

--- model.py
+++ model.py
@@ -153,7 +153,7 @@ def train_model(token):
     y = df['close'].values  # Target: closing prices
     
     #model = SVR(kernel='poly', degree=3)
-    model = SVR(kernel='sigmoid')
+    model = SVR(kernel='rbf')
     model.fit(X, y)
 
     next_time_index = np.array([[len(df)]])  # Giá trị thời gian tiếp theo
